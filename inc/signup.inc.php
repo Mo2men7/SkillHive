4d60bdda1629ad05439886be5cf7d6aa275460e7
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-03-21 23:43:53 +0200

    signup org page

---
+++ inc/signup.inc.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST["register"])) {
+    try {
+        echo "lol";
+        require_once "connect.php";
+        $stm = $connect->prepare("insert into organization(org_name,date_of_est,email,password,org_description,location) values (?,?,?,?,?,?)");
+        
+        $stm->execute([
+            $_POST["org_name"],
+            $_POST["date_of_est"], $_POST["email"],
+            $_POST["password"],
+            $_POST["org_description"],
+            $_POST["location"]
+        ]);
+        header("Location: ../success.php");
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
