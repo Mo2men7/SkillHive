0b60d9cd076f5d96856e4fdb08c984416e6cc72e
Amira-Elsayed-Abdelaziz <amira.elsayed.abdelaziz@gmail.com>
2024-03-30 21:55:38 +0200

    Edit Job and listing Applicants

--- inc/function.inc.php
+++ inc/function.inc.php
@@ -1,8 +1,11 @@
 <?php
-function jobs($org_id)
+
+require_once "connect.php";
+$_SESSION["jobs"] = jobs($connect, $_SESSION["id_org"]);
+function jobs($connect, $org_id)
 {
-    require_once "connect.php";
-    $stmjob = $connect->prepare("SELECT * FROM jobs where jobs.org_id=?;
+    // require_once "connect.php";
+    $stmjob = $connect->prepare("SELECT jobs.* ,category.category FROM jobs inner join category on jobs.id_category = category.id_category where jobs.org_id=?;
    ");
 
     $stmjob->execute([
@@ -10,67 +13,25 @@ function jobs($org_id)
     ]);
 
     $resjob = $stmjob->fetchAll();
-    // echo "<pre>";
-    // print_r($resjob);
-    // echo "</pre>";
-    $newjobarray = $resjob;
-    $stmcat = $connect->prepare("SELECT jobs.id_job , category.category FROM jobs LEFT JOIN job_category ON job_category.id_job = jobs.id_job LEFT JOIN category ON job_category.id_category = category.id_category where jobs.org_id=?;
-   ");
+    $fixingId = array();
+    foreach ($resjob as $job) {
+        $fixingId[$job["id_job"]] = $job;
+        $stmapp = $connect->prepare("SELECT applicant.* , job_app.* from applicant inner join job_app on applicant.id_app=job_app.id_app where job_app.id_job=?;");
 
-    $stmcat->execute([
-        $org_id
-    ]);
-
-    $rescat = $stmcat->fetchAll();
-
-    foreach ($resjob as $jobkey => $jobinfo) {
-        // echo "<pre>";
-        // print_r($jobinfo);
-        // echo "</pre>";
-        // echo $jobinfo["id_job"];
-
-        foreach ($rescat as $key => $value) {
-            if ($value["id_job"] == $jobinfo["id_job"]) {
-                // echo " works";
-                if (!isset($resjob[$jobkey]["category"]))
-                    $resjob[$jobkey]["category"] = array();
-                array_push($resjob[$jobkey]["category"], $value["category"]);
-            }
-            // echo "<pre>";
-            // print_r($value);
-            // echo "</pre>";
+        $stmapp->execute([
+            $job["id_job"]
+        ]);
+        $resapp = $stmapp->fetchAll();
+        foreach ($resapp as $app) {
+            $fixingId[$job["id_job"]]["applicants"][$app["id_app"]] = $app;
         }
     }
-    // echo "<pre>";
-    // print_r($resjob);
-    // echo "</pre>";
-    return $resjob;
+    return $fixingId;
 }
-     
-    // foreach($res as $key => $value)
-    // {
-    // $stmjobcat = $connect->prepare("SELECT * FROM job_category
-
-    // WHERE id_job=?;");
-
-    // $stmcat->execute([
-    //     $value["id_job"]
-    // ]);
-
-//    $res= $stmjob->fetchAll();
-//    echo "<pre>";
-//    print_r($res) ; 
-//    echo "</pre>";
-
-        
-//     print_r($value["id_job"]) ; 
-    
-//     } 
-// }
-// SELECT jobs.*, category.category   FROM (
-//     (jobs  INNER JOIN job_category as jc ON jc.id_job = jobs.id_job ) INNER JOIN
-//     (job_category INNER JOIN category ON job_category.id_category = category.id_category)) where jobs.org_id = 1 ;
-    
-    
-    
-// (select job_category.id_job , category.category from  job_category INNER join category on  job_category.id_category = category.id_category)
+function getCategory($connect)
+{
+    $stm = $connect->prepare("SELECT * FROM category;");
+    $stm->execute();
+    $res = $stm->fetchAll();
+    return $res;
+}
